7ffb2104888ac29e7e094ddb150ee2e5eef43c40
Saibal1234 <saibalroy587@gmail.com>
2026-02-27 23:21:54 +0530

    Fix API baseURL for deployment

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,14 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
+// Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
